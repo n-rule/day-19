5fc4ca2dd013641e185cf382d68816b481f9380a
n-rule <rule@ukr.net>
2021-01-26 15:21:02 +0200

    1

--- turtle_race_main.py
+++ turtle_race_main.py
@@ -1,7 +1,31 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=600, height=400)
+user_bet = screen.textinput('Make your bet', 'Which turtle will win the race? ')
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+all_turtles = []
+
+height = -100
+
+for turtle_index in range(6):
+    new_turtle = Turtle('turtle')
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.setpos(-270, height)
+    height += 35
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
 
 
 screen.exitonclick()
